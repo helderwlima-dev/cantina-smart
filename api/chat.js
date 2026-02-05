eb7110067001a663a6f1467528f001d5132dfaae
helderwlima-dev <helderwlima@gmail.com>
2026-02-05 09:08:05 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -16,10 +16,6 @@ export default async function handler(req, res) {
   }
 
   try {
-    return res.status(200).json({
-  chaveExiste: !!process.env.GEMINI_API_KEY,
-  tamanhoDaChave: process.env.GEMINI_API_KEY?.length || 0
-});
 
     const { mensagem } = req.body;
 
